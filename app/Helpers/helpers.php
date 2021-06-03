b039b5abe0fb17f65aae6a5826c646834fb14783
Milzam Hutomo <milzamhutomo.social@gmail.com>
2021-06-03 17:11:42 +0700

    Create dateIDN function

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,32 @@
+<?php 
+
+function dateIDN($date){
+    $day = date('d', strtotime($date));
+    (int) $n_month = date('n', strtotime($date));
+    $year = date('Y', strtotime($date));
+
+    $months = array(
+        "Januari"   => 1,
+        "Februari"  => 2,
+        "Maret"     => 3,
+        "April"     => 4,
+        "Mei"       => 5,
+        "Juni"      => 6,
+        "Juli"      => 7,
+        "Agustus"   => 8,
+        "September" => 9,
+        "Oktober"   => 10,
+        "November"  => 11,
+        "Desember"  => 12
+    );
+
+    $month = array_search($n_month, $months);
+
+    return $day . " " . $month . " " . $year;
+}
+
+function futureDate($date, int $day){
+
+}
+
+?>
